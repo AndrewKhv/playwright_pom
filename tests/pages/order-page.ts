573123a26ecaa4afdafbc9d914e734fd1f663ed1
AndrewKhv <44403470+AndrewKhv@users.noreply.github.com>
2025-03-14 01:52:39 +0700

    feat: TL-22 lesson 22

--- tests/pages/order-page.ts
+++ tests/pages/order-page.ts
@@ -1,4 +1,4 @@
-import { Page } from '@playwright/test'
+import { expect, Locator, Page } from '@playwright/test'
 import { Input } from '../atoms/Input'
 import { Button } from '../atoms/Button'
 
@@ -8,6 +8,11 @@ export class OrderPage {
   readonly nameField: Input;
   readonly phoneField: Input;
   readonly commentField: Input;
+  readonly createOrderButton: Button;
+  readonly notificationPopupText: Locator;
+  readonly notificationPopupClose: Button;
+  readonly searchOrderInput: Input;
+  readonly trackingButton: Button;
 
   constructor(page: Page) {
     this.page = page
@@ -15,5 +20,14 @@ export class OrderPage {
     this.nameField = new Input(page, '#name')
     this.phoneField = new Input(page, '#phone')
     this.commentField = new Input(page, '#comment')
+    this.createOrderButton = new Button(page, '.new-order__button')
+    this.notificationPopupText = page.locator('span.notification-popup__text')
+    this.notificationPopupClose = new Button(page, '[data-name="orderSuccessfullyCreated-popup-ok-button"]')
+    this.searchOrderInput = new Input(page, '[data-name="searchOrder-input"]')
+    this.trackingButton = new Button(page, '[data-name="searchOrder-submitButton"]')
+  }
+
+  async checkCreatedOrderID(id: number): Promise<void> {
+    expect(await this.notificationPopupText.innerText()).toContain(`${id}`);
   }
 }
--- tests/ui/e2e-flow-simple.spec.ts
+++ tests/ui/e2e-flow-simple.spec.ts
@@ -20,4 +20,84 @@ test('login with correct credentials and verify order creation page', async ({ p
   await orderCreationPage.nameField.checkVisible();
 })
 
-// ztd4sqbYbfT2n
+test('TL-22-1 Mocked auth', async ({ page }) => {
+  const loginPage = new LoginPage(page)
+  await loginPage.open()
+  await page.route('**/login/student', async route => {
+    const responseBody = 'test.test.test'
+    await route.fulfill({
+      status: 200,
+      contentType: 'application/json',
+      body: responseBody
+    })
+  })
+  const orderCreationPage = await loginPage.signIn(USERNAME, PASSWORD)
+  await orderCreationPage.statusButton.checkDisabled(false);
+  await orderCreationPage.nameField.checkVisible();
+})
+
+test('TL-22-2 Mocked auth + order creation', async ({ page }) => {
+  const name = 'qwertyyywefbfg';
+  const phone = 'fwerfwergwergwe';
+  const comment = 'fwfqefqwefq';
+  const orderId = 79789879;
+  const responseBody = {
+    status: 'OPEN',
+    courierId: null,
+    customerName: name,
+    customerPhone: phone,
+    comment: comment,
+    id: orderId
+  };
+
+  const loginPage = new LoginPage(page)
+  await loginPage.open()
+  await page.route('**/login/student', async route => {
+    const responseBody = 'test.test.test'
+    await route.fulfill({
+      status: 200,
+      contentType: 'application/json',
+      body: responseBody
+    })
+  })
+  const orderCreationPage = await loginPage.signIn(USERNAME, PASSWORD);
+  await orderCreationPage.statusButton.checkDisabled(false);
+  await orderCreationPage.nameField.checkVisible();
+  await orderCreationPage.nameField.fill(name);
+  await orderCreationPage.phoneField.fill(phone);
+  await orderCreationPage.commentField.fill(comment);
+  await orderCreationPage.createOrderButton.checkDisabled(false);
+  await page.route('**/orders**', async route => {
+    const method = route.request().method();
+    switch (method) {
+      case 'POST': {
+        return await route.fulfill({
+          status: 200,
+          contentType: 'application/json',
+          body: JSON.stringify(responseBody)
+        })
+      }
+      case 'GET': {
+        return await route.fulfill({
+          status: 200,
+          contentType: 'application/json',
+          body: JSON.stringify(responseBody)
+        })
+      }
+      default: {
+        return await route.continue();
+      }
+    }
+  })
+  const createOrderResponse = page.waitForResponse(response => response.url().includes('orders') && response.request().method() === 'POST');
+  await orderCreationPage.createOrderButton.click();
+  await createOrderResponse;
+  await orderCreationPage.checkCreatedOrderID(orderId);
+  await orderCreationPage.notificationPopupClose.click();
+  await orderCreationPage.statusButton.click();
+  await orderCreationPage.searchOrderInput.fill(`${orderId}`);
+  const searchOrderResponse = page.waitForResponse(response => response.url().includes('orders') && response.request().method() === 'GET')
+  await orderCreationPage.trackingButton.click();
+  await searchOrderResponse;
+  expect(page.url().includes(`${orderId}`)).toBeTruthy();
+})
